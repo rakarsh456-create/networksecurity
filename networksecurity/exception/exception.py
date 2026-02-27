0e9c2bb2ddf17f9af795c0440cedf72a4a04082d
Akarsh <rakarsh456@gmail.com>
2026-02-27 21:08:50 +0530

    ETL pipeline completed

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -11,4 +11,4 @@ class NetworkSecurityException(Exception):
     def __str__(self):
         return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         self.file_name, self.lineno, str(self.error_message))
-    
+     
