b2a63577fb3303de11aad854358725b8d2e524d9
Akarsh <rakarsh456@gmail.com>
2026-02-28 17:10:43 +0530

    Data ingestion component completed

--- push_data.py
+++ push_data.py
@@ -49,7 +49,7 @@ class NetworkDataExtract():
             raise NetworkSecurityException(e,sys) 
         
 if __name__=='__main__':
-    FILE_PATH="Network_Data\Dataset Phising Website.csv"
+    FILE_PATH="Network_Data\phishingData.csv"
     DATABASE="KRISHAI"
     Collection="NetworkData"
     networkobj=NetworkDataExtract()
