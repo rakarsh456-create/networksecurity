609b44accc3e348e287c075a60dab7452058c6eb
Akarsh <rakarsh456@gmail.com>
2026-03-03 12:24:54 +0530

    Performing DataTransformation

---
+++ networksecurity/components/data_transformation.py
@@ -0,0 +1,19 @@
+import sys
+import os
+import numpy
+import pandas as pd
+from sklearn.impute import KNNImputer
+from sklearn.pipeline import Pipeline
+
+from networksecurity.constant.training_pipeline import TARGET_COLUMN
+from networksecurity.constant.training_pipeline import DATA_TRANSFORMATION_IMPUTER_PARAMS
+
+from networksecurity.entity.artifact_entity import (
+    DataTransfromationArtifact,
+    DataValidationArtifact
+)
+
+from netwroksecurity.entity.config_entity import DataTransformationConfig
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from networksecurity.utils.main_utils.utils import save_numpy_array_data,save_object
